8ad5022eb98a915a727ad5a8ea408f7c0885abfb
SnowingWindsong <66971710+SnowingWindsong@users.noreply.github.com>
2022-10-05 01:13:05 -0700

    Time: 77 ms (33.66%), Space: 44.5 MB (81.29%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,11 +1,17 @@
 class Solution {
-     
+    
+ 
+    int[] pass = new int[]{0,0}; 
+    
+    
     public int[] twoSum(int[] nums, int target) {
         
         for(int i = 0; i<nums.length; i++){
           for(int j = i + 1; j<nums.length; j++){ 
             if( nums[i] + nums[j] == target){
-                return new int[]{i,j};
+                pass[0] = i;
+                pass[1] = j;
+                return pass;
                
             }
           }
